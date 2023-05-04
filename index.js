4e5bfbe8dc31990dbf6385cf562336bab3ea3bc9
Nightmaregodss <81895400+Nightmaregodss@users.noreply.github.com>
2023-05-04 12:10:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+      const axios = require('axios');
+      const config = require('./config.json')
 
+     const headers = {
+        'Authorization': `Bearer ${config.api}`,
+         'Content-Type': 'application/json'
+    };
+
+axios.get(API_URL + 'servers', { headers })
+    .then(response => {
+        const unsafeServers = response.data.data.filter(server => !server.attributes.name.includes(`${config.keyword}`));
+
+        unsafeServers.forEach(server => {
+            axios.delete(`${config.url}servers/${server.attributes.id}`, { headers })
+                .then(response => {
+                    console.log(`Server ${server.attributes.name} has been deleted.`);
+                })
+                .catch(error => {
+                    console.log(`An error occurred while deleting server ${server.attributes.name}: ${error}`);
+                });
+        });
+    })
+    .catch(error => {
+        console.log(`An error occurred while fetching servers: ${error}`);
+    });
