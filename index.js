a4f10036c5979e46774df3738b546dab64165972
Demon <81895400+Nightmaregodss@users.noreply.github.com>
2024-03-06 19:56:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-     const axios = require('axios');
-      const config = require('./config.json')
+const fetch = require('node-fetch');
+const config = require('./config.json');
 
-     const headers = {
+fetch(`${config.url}api/application/servers`, {
+    method: 'GET',
+    headers: {
         'Authorization': `Bearer ${config.api}`,
-         'Content-Type': 'application/json'
-    };
+        'Content-Type': 'application/json'
+    }
+})
+.then(response => response.json())
+.then(data => {
+    const getto = data.data.filter(server => !server.attributes.name.includes(config.keyword));
 
-axios.get(`${config.url}` + 'api/application/servers', { headers })
-    .then(response => {
-        const unsafeServers = response.data.data.filter(server => !server.attributes.name.includes(`${config.keyword}`));
-
-        unsafeServers.forEach(server => {
-            axios.delete(`${config.url}api/application/servers/${server.attributes.id}`, { headers })
-                .then(response => {
-                    console.log(`Server ${server.attributes.name} has been deleted.`);
-                })
-                .catch(error => {
-                    console.log(`An error occurred while deleting server ${server.attributes.name}: ${error}`);
-                });
+    getto.foreach(server => {
+        fetch(`${config.url}api/application/servers/${server.attributes.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${config.api}`,
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            console.log(`Server ${server.attributes.name} has been gased.`);
+        })
+        .catch(error => {
+            console.log(`An error allowed this server to exist ${server.attributes.name}: ${error}`);
         });
-    })
-    .catch(error => {
-        console.log(`An error occurred while fetching servers: ${error}`);
     });
+})
+.catch(error => {
+    console.log(`An error has slipped through our systems: ${error}`);
+});
